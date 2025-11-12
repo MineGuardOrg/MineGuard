1d0a02ffd6648fdfbb97ea55c0b72bd05bc65a9a
juan149902 <0322103675@ut-tijuana.edu.mx>
2025-11-11 22:02:39 -0800

    Es el avance de la landing page

--- web/src/app/components/footer/footer.component.ts
+++ web/src/app/components/footer/footer.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MaterialModule } from '../../material.module';
 import { CommonModule } from '@angular/common';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+interface FooterLink {
+  route: string;
+  label: string;
+}
+
+interface SocialLink {
+  url: string;
+  icon: string;
+  label: string;
+}
+
+interface FooterData {
+  SYSTEM: {
+    TITLE: string;
+    LINKS: FooterLink[];
+  };
+  DOCUMENTATION: {
+    TITLE: string;
+    LINKS: FooterLink[];
+  };
+  SOCIAL: {
+    TITLE: string;
+    LINKS: SocialLink[];
+  };
+}
 
 @Component({
   selector: 'app-footer',
@@ -19,18 +45,32 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   ],
   templateUrl: './footer.component.html',
 })
-export class AppFooterComponent {
+export class AppFooterComponent implements OnInit {
   
-  // Información del equipo basada en el SRS
-  teamMembers = [
-    'Balderas Martinez Vanessa',
-    'Avalos Garcia Juan Antonio', 
-    'Chávez Castillón Angel Alejandro',
-    'De Guerreroosio Arenas Isaac',
-    'Parra Espinosa Alexander'
-  ];
+  footerData: FooterData | null = null;
 
-  constructor(public translate: TranslateService) {}
+  constructor(public translate: TranslateService) {
+    // Cargar idioma guardado o usar español por defecto
+    const savedLang = localStorage.getItem('selectedLanguage') || 'es';
+    this.translate.setDefaultLang('es');
+    this.translate.use(savedLang);
+  }
+
+  ngOnInit(): void {
+    this.loadFooterData();
+    
+    // Escucha cambios de idioma y guarda la preferencia
+    this.translate.onLangChange.subscribe((event) => {
+      localStorage.setItem('selectedLanguage', event.lang);
+      this.loadFooterData();
+    });
+  }
+
+  loadFooterData(): void {
+    this.translate.get('FOOTER').subscribe(data => {
+      this.footerData = data;
+    });
+  }
 
   // Método para obtener el año actual
   getCurrentYear(): number {
